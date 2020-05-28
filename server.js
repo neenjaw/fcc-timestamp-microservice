f8e59b7b9cc3381918493f143339a29734fd5743
Tim Austin <tim@neenjaw.com>
2020-05-28 15:53:21 -0600

    formatted

--- server.js
+++ server.js
@@ -6,7 +6,7 @@
 1. The API endpoint is `GET [project_url]/api/timestamp/:date_string?`
 2. A date string is valid if can be successfully parsed by `new Date(date_string)` (JS) . Note that the unix timestamp needs to be an **integer** (not a string) specifying **milliseconds**. In our test we will use date strings compliant with ISO-8601 (e.g. `"2016-11-20"`) because this will ensure an UTC timestamp.
 3. If the date string is **empty** it should be equivalent to trigger `new Date()`, i.e. the service uses the current timestamp.
-4. If the date string is **valid** the api returns a JSON having the structure 
+4. If the date string is **valid** the api returns a JSON having the structure
 `{"unix": <date.getTime()>, "utc" : <date.toUTCString()> }`
 e.g. `{"unix": 1479663089000 ,"utc": "Sun, 20 Nov 2016 17:31:29 GMT"}`.
 5. If the date string is **invalid** the api returns a JSON having the structure `{"error" : "Invalid Date" }`.
@@ -27,7 +27,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -39,10 +39,10 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your timestamp API endpoint... 
+// your timestamp API endpoint...
 app.get("/api/timestamp/:datestring?", (req, res) => {
   const datestring = req.params.datestring
-  
+
   let date
   if (!datestring) {
     date = new Date()
@@ -51,17 +51,17 @@ app.get("/api/timestamp/:datestring?", (req, res) => {
   } else {
     date = new Date(Number(datestring))
   }
-  
+
   console.log({datestring, date})
-  
+
   if (date.toString() === 'Invalid Date') {
     res.json({error: 'Invalid Date'});
   } else {
     res.json({
       unix: date.getTime(),
       utc: date.toUTCString()
-    }); 
-  }  
+    });
+  }
 });
 
 // listen for requests :)
